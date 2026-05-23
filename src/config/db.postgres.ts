7ff96d922d8e33b1cbef1333ae21b931b9387d8e
Abdelrahman Emad <abdoemad0120@gmail.com>
2026-05-23 05:58:09 +0300

    feat: implement config for database ( pg & mongo )

---
+++ src/config/db.postgres.ts
@@ -0,0 +1,41 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
+
+
+const sequelize = new Sequelize(
+  PGDATABASE as string,
+  PGUSER as string,
+  PGPASSWORD as string,
+  {
+    host: (PGHOST as string) || "localhost",
+    port: Number(PGPORT) || 5432,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      preparedStatements: false,
+    },
+    pool: {
+      max: 2,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+);
+
+export default async () => {
+  await sequelize
+    .authenticate()
+    .then(() => console.log("Connected to PostgreSQL"))
+    .catch((error) => {
+      console.error("PostgreSQL connection error:", error);
+      process.exit(1);
+    });
+};
